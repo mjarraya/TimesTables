70d5f3299137dd39b09615bce3d3ea196a7d531b
mjarraya <mjarraya@student.42.fr>
2020-04-20 18:30:09 +0200

    Initial Commit

---
+++ TimesTables/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimesTables
+//
+//  Created by Montasar on 20/04/2020.
+//  Copyright © 2020 Montasar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
